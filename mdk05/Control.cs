3e0f91aef3eb29e35599da9d14027715f4e7df12
tyahseliy <bardaksss3@gmail.com>
2023-01-20 16:04:24 +0300

    Добавьте файлы проекта.

---
+++ mdk05/Control.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdk05
+{
+    public partial class Control : Form
+    {
+        private readonly string _role;
+        public Control(string role)
+        {
+            InitializeComponent();
+            _role = role;
+            label1.Text = _role;
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ mdk05/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdk05
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //client
+            var control = new Control(button1.Text);
+            control.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //realtor
+            var control = new Control(button2.Text);
+            control.ShowDialog();
+        }
+    }
+}
